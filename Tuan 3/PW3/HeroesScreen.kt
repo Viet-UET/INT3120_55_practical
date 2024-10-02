e69cb8d3512afde5a2fa1c8b6213c84141d41d77
Viet-UET <vietmtruong@gmail.com>
2024-10-02 16:57:18 +0700

    cm

---
+++ Tuan 3/PW3/HeroesScreen.kt
@@ -0,0 +1,67 @@
+package com.example.superheroes
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.model.Superhero
+import com.example.superheroes.ui.theme.Shapes
+import com.example.superheroes.ui.theme.Typography
+
+@Composable
+fun SuperheroItem(
+    superhero: Superhero, modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+    ) {
+        Row(
+
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium)),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(superhero.name),
+                    style = Typography.displaySmall,
+                )
+                Text(
+                    text = stringResource(superhero.description),
+                    style = Typography.bodyLarge,
+                )
+            }
+            Spacer(modifier = Modifier.width(dimensionResource(R.dimen.padding_medium)))
+            Image(
+                modifier = modifier
+                    .clip(Shapes.small)
+                    .size(dimensionResource(R.dimen.padding_X_large72))
+                    .padding(
+                        start = dimensionResource(R.dimen.padding_medium)
+                    ),
+                painter = painterResource(id = superhero.imageResourceId),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth
+            )
+        }
+    }
+}
---
+++ Tuan 3/PW3/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.superheroes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroes.data.Datasource
+import com.example.superheroes.ui.theme.SuperheroesTheme
+import com.example.superheroes.ui.theme.Typography
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperheroesTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SuperHeroesApp()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SuperHeroesApp() {
+    val superHeroData = Datasource().loadSuperheroes();
+
+    Scaffold(topBar = { SuperHeroesTopAppBar() }) { it ->
+        LazyColumn(
+            contentPadding = it,
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+            modifier = Modifier.padding(
+                start = dimensionResource(R.dimen.padding_medium),
+                end = dimensionResource(R.dimen.padding_medium)
+            )
+        ) {
+            items(superHeroData) {
+                SuperheroItem(
+                    superhero = it
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SuperHeroesTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(title = {
+        Text(
+            text = stringResource(id = R.string.app_name), style = Typography.displayLarge
+        )
+    }, modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SuperheroesTheme {
+        SuperHeroesApp()
+    }
+}
