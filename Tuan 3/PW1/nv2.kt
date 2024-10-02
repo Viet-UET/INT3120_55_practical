b73186044a253515febc562b3fbea7c65d320aa0
Viet-UET <vietmtruong@gmail.com>
2024-10-02 12:27:52 +0700

    commit

---
+++ Tuan 3/PW1/nv2.kt
@@ -0,0 +1,24 @@
+package com.example.practice
+
+enum class Daypart {
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
+
+data class Event(
+    val title: String,
+    val description: String?,
+    val daypart: Daypart,
+    val durationInMinutes: Int
+)
+
+fun main() {
+    val event = Event(
+        title = "Study Kotlin",
+        description = "Commit to studying Kotlin at least 15 minutes per day.",
+        daypart = Daypart.EVENING,
+        durationInMinutes = 15
+    )
+    println(event)
+}
